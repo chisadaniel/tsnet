b5eff19a7aa33956d86b27e74aa5036697e8b75f
chisadaniel <chisa.dani@yahoo.com>
2020-05-05 01:56:26 +0300

    incercare 2

---
+++ gRPC_SI_EF_2/PostComment/ModelPostComment.edmx.sql
@@ -0,0 +1,85 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 05/05/2020 00:48:03
+-- Generated from EDMX file: D:\dotnet\grpc\examples\csharp\Helloworld\PostComment\ModelPostComment.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [PostComment];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'PostSet'
+CREATE TABLE [dbo].[PostSet] (
+    [PostId] int IDENTITY(1,1) NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [Domain] nvarchar(max)  NOT NULL,
+    [Date] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'CommentSet'
+CREATE TABLE [dbo].[CommentSet] (
+    [CommentId] int IDENTITY(1,1) NOT NULL,
+    [Text] nvarchar(max)  NOT NULL,
+    [PostPostId] nvarchar(max)  NOT NULL,
+    [Post_PostId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [PostId] in table 'PostSet'
+ALTER TABLE [dbo].[PostSet]
+ADD CONSTRAINT [PK_PostSet]
+    PRIMARY KEY CLUSTERED ([PostId] ASC);
+GO
+
+-- Creating primary key on [CommentId] in table 'CommentSet'
+ALTER TABLE [dbo].[CommentSet]
+ADD CONSTRAINT [PK_CommentSet]
+    PRIMARY KEY CLUSTERED ([CommentId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [Post_PostId] in table 'CommentSet'
+ALTER TABLE [dbo].[CommentSet]
+ADD CONSTRAINT [FK_PostComment]
+    FOREIGN KEY ([Post_PostId])
+    REFERENCES [dbo].[PostSet]
+        ([PostId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_PostComment'
+CREATE INDEX [IX_FK_PostComment]
+ON [dbo].[CommentSet]
+    ([Post_PostId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
